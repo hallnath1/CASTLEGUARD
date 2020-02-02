5cfa3992be7400c6cd9e7aedde3274d86052a49c
Graham <grahamk206@gmail.com>
2020-02-02 09:20:41 +0000

    Calculate InfoLoss and Enlargement values for a cluster

--- src/cluster.py
+++ src/cluster.py
@@ -5,7 +5,7 @@ class Cluster():
     def __init__(self):
         """Initialises the cluster """
         self.contents = []
-        self.ranges = []
+        self.ranges = {}
 
     def insert(self, element):
         """Inserts a tuple into the cluster
@@ -17,8 +17,9 @@ class Cluster():
 
         """
         self.contents.append(element)
+        # TODO: update ranges using element headers
 
-    def enlargement(self, t):
+    def enlargement(self, t, global_ranges):
         """Calculates the enlargement value for adding <t> into this cluster
 
         Args:
@@ -27,9 +28,12 @@ class Cluster():
         Returns: TODO
 
         """
-        pass
 
-    def information_loss(self, t):
+        info_loss = self.information_loss_given(t, global_ranges) - self.information_loss(global_ranges)
+
+        return info_loss / len(self.ranges)
+
+    def information_loss_given(self, t, global_ranges):
         """Calculates the information loss upon adding <t> into this cluster
 
         Args:
@@ -38,7 +42,28 @@ class Cluster():
         Returns: TODO
 
         """
-        pass
+        # TODO: can be optimised by looping through ranges and calculating infoLoss on ranges that would be updated
+
+        ranges_copy = self.ranges.copy()
+        self.insert(t)
+        loss = self.information_loss(global_ranges)
+        self.contents.remove(t)
+        self.ranges = ranges_copy
+
+        return loss
+
+    def information_loss(self, global_ranges):
+        """Calculates the information loss of this cluster
+
+        Returns: information loss of cluster
+
+        """
+        loss = 0
+        for k, r in ranges.items():
+            global_range = global_ranges[k]
+            loss += r / global_range
+
+        return loss
 
     def __len__(self):
         return len(self.contents)
