77a617c1431d5b40f6cc76879654ff801f2054c5
Alexander Jackson <alexanderjackson@protonmail.com>
2020-02-02 20:15:22 +0000

    Implement clustering for CASTLE with visualisation
    Implement the ability for data to be entered through the system and clustered.
    Once finished, the program will display the clusters it has created and the
    tuples contained inside them.

--- src/main.py
+++ src/main.py
@@ -1,22 +1,45 @@
 import time
 
 import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
 
 from castle import CASTLE
 
 def handler(value):
     print("RECIEVED VALUE: {}".format(value))
 
+def create_rectangle(rx, ry):
+    width = rx.upper - rx.lower
+    height = ry.upper - ry.lower
+    return patches.Rectangle((rx.lower, ry.lower), width, height, fill=False)
+
 def main():
-    frame = pd.read_csv("data.csv")
+    frame = pd.read_csv("data.csv").sample(10)
 
     headers = list(frame.columns.values)
     stream = CASTLE(handler, headers, 5, 10, 5)
 
     for (_, row) in frame.iterrows():
-        print("INSERTING: {}".format(row))
         stream.insert(row)
-        time.sleep(0.5)
+        # time.sleep(0.5)
+
+    fig, ax = plt.subplots(1)
+
+    for cluster in stream.big_gamma:
+        print([str(r) for r in cluster.ranges.values()])
+        plid_range = cluster.ranges["PickupLocationID"]
+        distance_range = cluster.ranges["TripDistance"]
+
+        ax.add_patch(create_rectangle(plid_range, distance_range))
+
+        for (_, t) in cluster.contents.iterrows():
+            pickup_id = t["PickupLocationID"]
+            distance = t["TripDistance"]
+            print(pickup_id, distance)
+            plt.scatter(pickup_id, distance)
+
+    plt.show()
 
 if __name__ == "__main__":
     main()
