0a9515612e6c5770fa8b4023a26617bf248838fa
Graham <grahamk206@gmail.com>
2020-02-13 20:06:05 +0000

    Remove TODO comments in ITEM

--- src/item.py
+++ src/item.py
@@ -12,16 +12,6 @@ class Item:
     def __str__(self):
         return self.data.to_string()
 
-    # TODO: This is a bad way of calculating distance as it is heavily biased
-    # on certain attributes
-    #
-    # For example, given the original tuple of A = (1, 100) and tuples B = (2,
-    # 110) and C = (3, 105), it will find that B is further from A than C is.
-    # This might be the desired behaviour however, as if the range of
-    # attributes is [1, 3] and [0, 1000], we might want C to be further as it
-    # is further along its global range.
-    #
-    # tuple_distance should use root mean square
     def tuple_distance(self, t):
         """Calculates the distance between the two tuples
 
