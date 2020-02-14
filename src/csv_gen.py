265210c7051aa5002d957cba24bf881cf79c262b
Freddie Brown <fred@noser.net>
2020-02-14 17:23:36 +0000

    create new generate for simulating castle
    creates a new method which does similar things as last time, but will
    generate a user defined min and max for specified headers if they are
    numerical.

--- src/csv_gen.py
+++ src/csv_gen.py
@@ -19,18 +19,31 @@ import datetime
 import random
 import re
 
-def generate(name=None,rows=50, headers=["Name","Age", "Education", "GPA"], datatypes=["name", "i100", "edu", "f5"]):
+def create_file(name=None):
+    if not name:
+        filename = str(int(time.time()))
+    else:
+        filename = name
+    filename+=".csv"
+    f = open(filename, "w")
+    return f, filename
+
+def gen_rand_number(dt):
+    if re.search("(int)([0-9]*)",dt):
+        return random.randrange(int(dt.split("int")[1]))
+    elif re.search("(float)([0-9]*)",dt):
+        return random.uniform(0,int(dt.split("float")[1]))
+    else:
+        return "0"
+
+
+def generate(name=None,rows=50, headers=["Name","Age", "Education", "GPA"], datatypes=["name", "int100", "edu", "float5"]):
     if len(headers) != len(datatypes):
         print("Need a datatype for each header")
         return
     fake = Faker()
     edu_levels = ["Primary", "Secondary", "Bachelors", "Masters", "PhD"]
-    if not name:
-        filename = str(int(time.time()))
-    else:
-        filename = name
-    filename += ".csv"
-    f = open(filename, "w")
+    f, filename = create_file(name)
     headers.insert(0,"pid")
     pid = 1
     f.write(','.join(headers)+"\n")
@@ -42,15 +55,53 @@ def generate(name=None,rows=50, headers=["Name","Age", "Education", "GPA"], data
                 row.append(fake.name())
             elif datatypes[i] == "edu":
                 row.append(edu_levels[(random.randrange(100)%6)-1])
-            elif re.search("([i][0-9]*)",datatypes[i]):
-                row.append(str(random.randrange(int(datatypes[i].split("i")[1]))))
-            elif re.search("([f][0-9]*)",datatypes[i]):
-                row.append(str(random.uniform(0,int(datatypes[i].split("f")[1]))))
+            elif re.search("(int|float)", datatypes[i]):
+                row.append(str(gen_rand_number(datatypes[i])))
+
         f.write(','. join(row)+"\n")
     return filename
 
+def generate_output_data(name=None, rows=50, headers=["Name", "Age", "Education", "GPA"], datatypes=["name", "int100", "edu", "float5"], generalise=["Age", "GPA"]):
+    if len(headers) != len(datatypes):
+        print("Need a datatype for each header")
+        return
+    fake = Faker()
+    edu_levels = ["Primary", "Secondary", "Bachelors", "Masters", "PhD"]
+    f, filename = create_file(name)
+    temp = []
+    temp.append("pid")
+    pid = 1
+    for i in range(0, len(headers)):
+        # In this loop, each header should be check if its in generalise
+        # If it is, create a min and a max 
+        if headers[i] in generalise:
+            temp.append("min"+headers[i])
+            temp.append("max"+headers[i])
+        else:
+            temp.append(headers[i])
+    f.write(','.join(temp)+"\n")
+    
+    for pid in range(0, rows):
+        row = [str(pid)]
+        for i in range(0, len(headers)):
+            if datatypes[i] == "name":
+                row.append(fake.name())
+            elif datatypes[i] == "edu":
+                row.append(edu_levels[(random.randrange(100)%6)-1])
+            elif re.search("(int|float)", datatypes[i]):
+                max = gen_rand_number(datatypes[i])
+                if headers[i] in generalise:
+                    min = random.uniform(0, max)
+                    if type(max) is int:
+                        min = int(min)
+                    row.append(str(min))
+                row.append(str(max))
+        f.write(','. join(row)+"\n")        
+    return filename
+
+
 if __name__ == "__main__":
-    filename = generate(name="test")
+    filename = generate_output_data(name="test")
     print(filename)
 
 
--- src/test_ml.py
+++ src/test_ml.py
@@ -1,21 +1,24 @@
 import pandas as pd
-import app
 from castle import CASTLE
+import csv_gen
 
 
 def handler(value: pd.Series):
-    print("RECIEVED VALUE: {}".format(value))
+	print("RECIEVED VALUE: {}".format(value))
 
-if __name__= "__main__":
-    args = app.parse_args()
-    
-    frame = pd.read_csv(args.filename).sample(20)
-    
-    headers = list(frame.columns.values)
-    
-    stream = CASTLE(handler, headers, args.k, args.delta, args.beta)
-    
-    for(_, row) in frame.iterrows():
-        stream.insert(row)
+if __name__=="__main__":
+
+	
+	# frame = pd.read_csv(args.filename).sample(20)
+	
+	# headers = list(frame.columns.values)
+	
+	# stream = CASTLE(handler, headers, args.k, args.delta, args.beta)
+	
+	# for(_, row) in frame.iterrows():
+	# 	stream.insert(row)
+
+	frame = pd.read_csv(csv_gen.generate_output_data("test")).sample(20)
+	print(frame)
 
 
