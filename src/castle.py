41ff064a2de4b712b7b347700185708b156429c0
Alexander Jackson <alexanderjackson@protonmail.com>
2020-03-09 21:47:12 +0000

    Change default l to 1 and remove total_diversity
    Change the default value of l to be 1 and remove the total_diversity variable
    as it is an odd naming system. Change total_cluster_size to be total_tuples as
    well as this is a better name for it.

--- src/castle.py
+++ src/castle.py
@@ -248,15 +248,13 @@ class CASTLE():
         if m > len(self.big_gamma) / 2:
             return self.suppress_tuple(t)
 
+        total_tuples = sum([len(cluster) for cluster in self.big_gamma])
         diversity_values = set()
 
         for cluster in self.big_gamma:
             diversity_values.update(cluster.diversity)
 
-        total_diversity = self.l < len(diversity_values)
-        total_cluster_size = sum([len(cluster) for cluster in self.big_gamma])
-
-        if total_cluster_size < self.k or not total_diversity:
+        if total_tuples < self.k or len(diversity_values) < self.l:
             return self.suppress_tuple(t)
 
         mc = self.merge_clusters(t.parent)
