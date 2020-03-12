5e75b4297a4341db215ab0b5b36ca1a792501632
Alexander Jackson <alexanderjackson@protonmail.com>
2020-03-12 16:33:53 +0000

    Add documentation to Cluster and Pylint it
    Add documentation to Cluster and fix all Pylint warnings, bringing it to a
    beautiful 10/10 Pylint score.

--- src/cluster.py
+++ src/cluster.py
@@ -1,8 +1,17 @@
+"""Store tuples that are considered by CASTLE to be together.
+
+Allow for operations on clusters of tuples such as `insert`, `remove` and
+`__contains__`. Allow for queries such as the information loss of a cluster,
+and the information loss of inserting tuples or merging clusters. Allow for
+generalisation of tuples with respect to the cluster itself.
+"""
+
 import copy
+
+from typing import Any, Dict, List, Set
+
 import numpy as np
 import pandas as pd
-
-from typing import Dict, List, Set, Any
 
 from item import Item
 from range import Range
@@ -12,14 +21,14 @@ class Cluster():
     """Stores tuples that are considered by the algorithm to be together. """
 
     def __init__(self, headers: List[str]):
-        """Initialises the cluster """
+        """Initialises the cluster. """
         self.contents: List[Item] = []
         self.ranges: Dict[str, Range] = {}
 
         for header in headers:
             self.ranges[header] = Range()
 
-        self.diversity : Set[Any] = set()
+        self.diversity: Set[Any] = set()
 
         self.sample_values: Dict[str, float] = {}
 
@@ -37,14 +46,14 @@ class Cluster():
             # If it is, remove it so that we do not reach an invalid state
             element.parent.remove(element)
 
-        # add sensitive attribute value to the diversity of cluster
+        # Add sensitive attribute value to the diversity of cluster
         self.diversity.add(element.sensitive_attr)
 
         # Update the parent of the item to be this cluster
         element.parent = self
 
-        for k, v in self.ranges.items():
-            v.update(element[k])
+        for header, header_range in self.ranges.items():
+            header_range.update(element[header])
 
     def remove(self, element: Item):
         """Removes a tuple from the cluster
@@ -58,109 +67,109 @@ class Cluster():
         if not element.sensitive_attr in [e.sensitive_attr for e in self.contents]:
             self.diversity.remove(element.sensitive_attr)
 
-    def generalise(self, t: Item) -> pd.Series:
+    def generalise(self, item: Item) -> pd.Series:
         """Generalises a tuple based on the ranges for this cluster
 
         Args:
-            t (Item): The tuple to be generalised
+            item (Item): The tuple to be generalised
 
         Returns: A generalised version of the tuple based on the ranges for
         this cluster
 
         """
-        gen_tuple = copy.deepcopy(t)
+        gen_tuple = copy.deepcopy(item)
 
-        for h, v in self.ranges.items():
+        for header, header_range in self.ranges.items():
             # Pick a random person to use for this attribute
-            if not h in self.sample_values:
-                self.sample_values[h] = np.random.choice(self.contents)[h]
+            if not header in self.sample_values:
+                self.sample_values[header] = np.random.choice(self.contents)[header]
 
-            gen_tuple.data.loc['min' + h] = v.lower
-            gen_tuple.data.loc['spc' + h] = self.sample_values[h]
-            gen_tuple.data.loc['max' + h] = v.upper
+            gen_tuple.data.loc['min' + header] = header_range.lower
+            gen_tuple.data.loc['spc' + header] = self.sample_values[header]
+            gen_tuple.data.loc['max' + header] = header_range.upper
 
-            gen_tuple.headers.append('min' + h)
-            gen_tuple.headers.append('spc' + h)
-            gen_tuple.headers.append('max' + h)
+            gen_tuple.headers.append('min' + header)
+            gen_tuple.headers.append('spc' + header)
+            gen_tuple.headers.append('max' + header)
 
-            gen_tuple.headers.remove(h)
-            del gen_tuple.data[h]
+            gen_tuple.headers.remove(header)
+            del gen_tuple.data[header]
 
         del gen_tuple.data['pid']
 
-        return gen_tuple, t
+        return gen_tuple, item
 
-    def tuple_enlargement(self, t: Item, global_ranges: Dict[str, Range]) -> float:
-        """Calculates the enlargement value for adding <t> into this cluster
+    def tuple_enlargement(self, item: Item, global_ranges: Dict[str, Range]) -> float:
+        """Calculates the enlargement value for adding <item> into this cluster
 
         Args:
-            t: The tuple to calculate enlargement based on
+            item: The tuple to calculate enlargement based on
             global_ranges: The globally known ranges for each attribute
 
-        Returns: The information loss if we added t into this cluster
+        Returns: The information loss if we added item into this cluster
 
         """
+        given = self.information_loss_given_t(item, global_ranges)
+        current = self.information_loss(global_ranges)
 
-        info_loss = self.information_loss_given_t(t, global_ranges) - self.information_loss(global_ranges)
+        return (given - current) / len(self.ranges)
 
-        return info_loss / len(self.ranges)
-
-    def cluster_enlargement(self, c, global_ranges: Dict[str, Range]) -> float:
-        """Calculates the enlargement value for merging <c> into this cluster
+    def cluster_enlargement(self, cluster: Cluster, global_ranges: Dict[str, Range]) -> float:
+        """Calculates the enlargement value for merging <cluster> into this cluster
 
         Args:
-            c: The cluster to calculate information loss for
+            cluster: The cluster to calculate information loss for
             global_ranges: The globally known ranges for each attribute
 
-        Returns: The information loss upon merging c with this cluster
+        Returns: The information loss upon merging cluster with this cluster
 
         """
+        given = self.information_loss_given_c(cluster, global_ranges)
+        current = self.information_loss(global_ranges)
 
-        info_loss = self.information_loss_given_c(c, global_ranges) - self.information_loss(global_ranges)
+        return (given - current) / len(self.ranges)
 
-        return info_loss / len(self.ranges)
-
-    def information_loss_given_t(self, t: Item, global_ranges: Dict[str, Range]) -> float:
-        """Calculates the information loss upon adding <t> into this cluster
+    def information_loss_given_t(self, item: Item, global_ranges: Dict[str, Range]) -> float:
+        """Calculates the information loss upon adding <item> into this cluster
 
         Args:
-            t: The tuple to calculate information loss based on
+            item: The tuple to calculate information loss based on
             global_ranges: The globally known ranges for each attribute
 
-        Returns: The information loss given that we insert t into this cluster
+        Returns: The information loss given that we insert item into this cluster
 
         """
         loss = 0
 
-        # For each range, check if <t> would extend it
-        for k, r in self.ranges.items():
-            global_range = global_ranges[k]
+        # For each range, check if <item> would extend it
+        for header, header_range in self.ranges.items():
+            global_range = global_ranges[header]
             updated = Range(
-                lower=min(r.lower, t.data[k]),
-                upper=max(r.upper, t.data[k])
+                lower=min(header_range.lower, item.data[header]),
+                upper=max(header_range.upper, item.data[header])
             )
             loss += updated / global_range
 
         return loss
 
-    def information_loss_given_c(self, c, global_ranges: Dict[str, Range]) -> float:
-        """Calculates the information loss upon merging <c> into this cluster
+    def information_loss_given_c(self, cluster: Cluster, global_ranges: Dict[str, Range]) -> float:
+        """Calculates the information loss upon merging <cluster> into this cluster
 
         Args:
-            c: The cluster to calculate information loss based on
+            cluster: The cluster to calculate information loss based on
             global_ranges: The globally known ranges for each attribute
 
-        Returns: The information loss given that we merge c with this cluster
+        Returns: The information loss given that we merge cluster with this cluster
 
         """
         loss = 0
 
-        # For each range, check if <t> would extend it
-        for k, r in self.ranges.items():
-            global_range = global_ranges[k]
+        # For each range, check if <item> would extend it
+        for header, header_range in self.ranges.items():
+            global_range = global_ranges[header]
             updated = Range(
-                lower=min(r.lower, c.ranges[k].lower),
-                upper=max(r.upper, c.ranges[k].upper)
+                lower=min(header_range.lower, cluster.ranges[header].lower),
+                upper=max(header_range.upper, cluster.ranges[header].upper)
             )
             loss += updated / global_range
 
@@ -176,33 +185,41 @@ class Cluster():
 
         """
         loss = 0
-        for k, r in self.ranges.items():
-            global_range = global_ranges[k]
-            loss += r / global_range
+
+        for header, header_range in self.ranges.items():
+            global_range = global_ranges[header]
+            loss += header_range / global_range
 
         return loss
 
-    def distance(self, t: Item):
-        # TODO: can be updated to use global ranges
+    def distance(self, other: Item) -> float:
+        """Calculates the distance from this tuple to another
 
+        Args:
+            other: The tuple to calculate the distance to
+
+        Returns: The distance to the other tuple
+
+        """
         total_distance = 0
-        for header, range in self.ranges.items():
-            total_distance += abs(t[header] - (range.upper - range.lower))
+
+        for header, header_range in self.ranges.items():
+            total_distance += abs(other[header] - (header_range.difference()))
 
         return total_distance
 
-    def within_bounds(self, t: Item) -> bool:
+    def within_bounds(self, item: Item) -> bool:
         """Checks whether a tuple is within all the ranges of the this
         cluster, eg. would cause no information loss on being entered.
 
         Args:
-            t: The tuple to perform bounds checking on
+            item: The tuple to perform bounds checking on
 
         Returns: Whether or not the tuple is within the bounds of the cluster
 
         """
-        for k, v in self.ranges.items():
-            if not v.within_bounds(t[k]):
+        for header, header_range in self.ranges.items():
+            if not header_range.within_bounds(item[header]):
                 return False
 
         return True
@@ -212,18 +229,18 @@ class Cluster():
         Returns: The number of tuples currently contained in the cluster
 
         """
-        return len({t['pid'] for t in self.contents})
+        return len({item['pid'] for item in self.contents})
 
-    def __contains__(self, t: Item) -> bool:
-        """Checks whether this cluster contains t
+    def __contains__(self, item: Item) -> bool:
+        """Checks whether this cluster contains item
 
         Args:
-            t: The tuple to find
+            item: The tuple to find
 
-        Returns: Whether or not this cluster contains t
+        Returns: Whether or not this cluster contains item
 
         """
-        return t in self.contents
+        return item in self.contents
 
     def __str__(self) -> str:
         """Creates a string representation of the cluster
